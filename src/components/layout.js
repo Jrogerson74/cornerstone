d35d2f672e90ff3b6693dbcdcb37f4fb69b275e9
Jrogerson74 <jrsquish@gmail.com>
2020-04-09 11:12:26 -0600

    styled header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+import Header from './headernav/header';
+import Navbar from './headernav/navbar';
+
 class Layout extends Component {
   render() {
     return (
-      <div>
+      <div className='layout'>
         {this.props.children}
+        <Header />
+        <Navbar />
       </div>
     );
   }
